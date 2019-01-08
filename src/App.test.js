aa7aae8e362547a355f24033794904da5441c7b7
dmithamo1 <dennis.mithamo@andela.com>
2019-01-08 15:46:50 +0300

    chore(setup testing env): configure the testing environment - locally - on CircleCI [Delivers #161966967]

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+/* Define a sample test to test CircleCi and
+Code Climate integration */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
