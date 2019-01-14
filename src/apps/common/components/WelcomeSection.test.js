f9b4e12c1490c78119d0e8a32e80f8da7c0a82be
EmmanuelChayu <emmanuel.chayu@andela.com>
2019-01-15 01:33:30 +0300

    ft(Login): implement login feature  - Add login form component  - Save data to redux store  - Display Success and Failure messages  - Add tests
    [Finishes  #161966974]

--- src/apps/common/components/WelcomeSection.test.js
+++ src/apps/common/components/WelcomeSection.test.js
@@ -6,6 +6,8 @@ import Adapter from 'enzyme-adapter-react-16';
 
 import WelcomeSection from './WelcomeSection';
 
+import FormInput from './FormInput';
+
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -13,6 +15,14 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('WelcomeSection', () => {
   it('should render correctly in "debug" mode', () => {
     const component = shallow(<WelcomeSection debug />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe('FormInput', () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<FormInput debug />);
 
     expect(component).toMatchSnapshot();
   });
